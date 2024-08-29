ae6322c8de55228ebaf701daf467ddd255381c80
Harshit Shukla <harshit.shukla005@gmail.com>
2024-08-29 15:06:59 +0530

    Contacting form

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+
+import styles from "./Button.module.css";
+
+export const Button = ({isOutline, icon, text, ...rest}) => {
+  return (
+
+    <button 
+    {...rest}
+    className= {isOutline ? styles.outline_btn : styles.primary_btn}>
+    
+        {icon}
+        {text}
+    </button>
+  );
+}
